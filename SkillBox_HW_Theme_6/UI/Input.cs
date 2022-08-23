954c8952cea3170d4bbea2d3e470186d35c3bee1
Gaifullin Damir <101250473+GaifullinDamir@users.noreply.github.com>
2022-08-23 20:36:38 +0300

    Добавьте файлы проекта.

---
+++ SkillBox_HW_Theme_6/UI/Input.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillBox_HW_Theme_6.UI
+{
+    internal class Input
+    {
+        private const int _maxValue = 1_000_000_000;
+        private const int _minValue = 0;
+
+        public static int InputInteger()
+        {
+            string input; bool stop = false;
+            int integer = -1;
+            while (!stop)
+            {
+                try
+                {
+                    input = Console.ReadLine();
+                    integer = int.Parse(input);
+                    if(integer >= _minValue && integer <= _maxValue)
+                    {
+                        stop = true;
+                    }
+                    else Console.WriteLine("Число не попадает в границы.");
+                    
+                }
+                catch (Exception)
+                {
+                    Console.Write("Введите целое число: ");
+                }
+            }
+            return integer;
+        }
+    }
+}
---
+++ SkillBox_HW_Theme_6/UI/UI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillBox_HW_Theme_6.UI
+{
+    internal class UI
+    {
+        
+        private int _n;
+    }
+}
